f99bfb5c6789ecc7c33e1e71031f5cac4840d298
Luis Gonzalez <luexigonzalez@gmail.com>
2026-01-20 00:10:25 -0600

    fix: optimize Vercel Edge Requests with static adapter

---
+++ .vercel/output/static/_astro/legacy.DAVDbcFn.js
@@ -0,0 +1,1 @@
+import{aq as e}from"./render.Cw_ZL3r4.js";const d="5";typeof window<"u"&&((window.__svelte??={}).v??=new Set).add(d);e();
--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mdo-steel.vercel.app',
+  output: 'static',
+  adapter: vercel(),
   integrations: [svelte()],
 
   vite: {
